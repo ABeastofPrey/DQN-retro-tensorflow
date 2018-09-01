38632cde669973e0cdd138037b376f35fae43a4a
Hsiu W Wang <hsiu.w.wang@cnsdc01.pwc.com>
2018-09-01 10:14:38 +0800

    modify argument parameter in  main.py

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import tensorflow as tf
 from src.inference import train, eval_play
 
 #第一个是参数名称，第二个参数是默认值，第三个是参数描述
-tf.app.flags.DEFINE_boolean('train', False, "whether do training or testing")
+tf.app.flags.DEFINE_integer('train', 1, "whether do training(1) or testing(0)")
 
 FLAGS = tf.app.flags.FLAGS
 
 def main(_):
     with tf.Session() as sess:
-        if FLAGS.train: 
+        if FLAGS.train == 1: 
             train(sess)
-        else: 
+        elif FLAGS.train == 0: 
             eval_play(sess)
+        else:
+            raise ValueError('train parameter should be 0 or 1')
 
 if __name__ == '__main__':
     tf.app.run()
